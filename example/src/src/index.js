238533dea06079b9a9a8b8b857bb3ec4dd231531
Sergey <landyrev.serg@gmail.com>
2022-08-22 20:33:17 +0300

    add endpoint to control metric

--- example/src/src/index.js
+++ example/src/src/index.js
@@ -3,9 +3,17 @@ const cors = require("cors");
 
 const server = express({});
 server.use(cors())
+server.use(express.json());
+
+let isHighload = false;
 
 server.get("/metrics", (req, res) => {
-    res.send(`example_random_metric: ${Math.random() * 10}`);
+    res.send(`example_random_metric: ${Math.random() * 5 + (isHighload ? 5 : 0)}`);
+});
+
+server.post("/highload", (req, res) => {
+    isHighload = req.body.isHighload;
+    res.send(`Is highload was saved, ${isHighload}`);
 });
 
 server.listen(3000, () => console.log("server was started"));
